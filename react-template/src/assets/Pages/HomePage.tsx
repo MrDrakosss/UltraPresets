66d1e2ddfb8178f861f5cfb68974d1f5da04d782
Tóth Dániel <91130208+tothdani561@users.noreply.github.com>
2025-06-06 19:45:06 +0200

    Basis React application

---
+++ react-template/src/assets/Pages/HomePage.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <>
+            <div className="h-screen flex items-center justify-center">
+                <h1 className="font-extrabold text-slate-600 text-5xl text-center">
+                    Basic React Application with Tailwindcss
+                </h1>
+            </div>
+        </>
+    )
+}
